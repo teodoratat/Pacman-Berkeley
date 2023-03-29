f60dc427a910bd1cd830ce96ab1a3b7607e72eea
Tat Teodora <teotat7@gmail.com>
2023-03-29 21:41:18 +0300

    add

---
+++ search.py
@@ -0,0 +1,298 @@
+# search.py
+# ---------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+#
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+In search.py, you will implement generic search algorithms which are called by
+Pacman agents (in searchAgents.py).
+"""
+
+from game import Directions
+import util
+
+class SearchProblem:
+    """
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
+
+    You do not need to change anything in this class, ever.
+    """
+
+    def getStartState(self):
+        """
+        Returns the start state for the search problem.
+        """
+        util.raiseNotDefined()
+
+    def isGoalState(self, state):
+        """
+          state: Search state
+
+        Returns True if and only if the state is a valid goal state.
+        """
+        util.raiseNotDefined()
+
+    def getSuccessors(self, state):
+        """
+          state: Search state
+
+        For a given state, this should return a list of triples, (successor,
+        action, stepCost), where 'successor' is a successor to the current
+        state, 'action' is the action required to get there, and 'stepCost' is
+        the incremental cost of expanding to that successor.
+        """
+        util.raiseNotDefined()
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.
+        The sequence must be composed of legal moves.
+        """
+        util.raiseNotDefined()
+
+
+def depthFirstSearch(problem):
+    """
+    Search the deepest nodes in the search tree first.
+
+    Your search algorithm needs to return a list of actions that reaches the
+    goal. Make sure to implement a graph search algorithm.
+
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+
+    print "Start:", problem.getStartState()
+    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    print "Start's successors:", problem.getSuccessors(problem.getStartState())
+
+    """
+    from util import Stack
+    stack = Stack()
+    visited = set()
+    actions = []
+
+
+    # Init stack and visited set
+    stack.push(((problem.getStartState(), problem.getSuccessors(problem.getStartState()))))
+    visited.add(problem.getStartState())
+
+    # Start DFS
+    while not stack.isEmpty():
+        cur_state, neighbor = stack.list[-1]
+        # Avoid repeat visitting same node
+        valid_neighbor = filter(lambda x: x[0] not in visited, neighbor)
+        for nxt in valid_neighbor:
+            # Check goal, if next node is goal, do not need to visit it
+            if problem.isGoalState(nxt[0]):
+                actions.append(nxt[1])
+                return actions
+            stack.push((nxt[0], problem.getSuccessors(nxt[0])))
+            actions.append(nxt[1])
+            visited.add((nxt[0]))
+            break
+        # If no more valid node to be visited, should pop that node from stack
+        if len(valid_neighbor) == 0:
+            stack.pop()
+            actions.pop()
+    # If no valid path can be found, return empty path
+    return []
+
+
+def breadthFirstSearch(problem):
+    """Search the shallowest nodes in the search tree first."""
+    from util import Queue
+    queue = Queue()
+    visited = set()
+
+    startState = problem.getStartState()
+
+    # Init queue and visited set
+    queue.push((startState, []))
+    visited.add(startState)
+
+    # Start BFS
+    while not queue.isEmpty():
+        cur_state, path = queue.pop()
+        if problem.isGoalState(cur_state):
+            return path
+        # Avoid repeat visitting same node
+        valid_neighbor = filter(lambda x: x[0] not in visited, problem.getSuccessors(cur_state))
+        for nxt in valid_neighbor:
+            queue.push((nxt[0], path + [nxt[1]]))
+            visited.add(nxt[0])
+    # If no valid path can be found, return empty path
+    return []
+
+
+def aStarSearch(problem, heuristic):
+    """Search the node that has the lowest combined cost and heuristic first."""
+    from util import PriorityQueue
+    pq = PriorityQueue()
+    res = dict()
+    visited = set()
+
+    # Init queue and visited set
+    pq.push(problem.getStartState(), heuristic(problem.getStartState(), problem))
+    res[problem.getStartState()] = []
+
+    # Start A*
+    while not pq.isEmpty():
+        cur_state = pq.pop()
+        path = res[cur_state]
+        visited.add(cur_state)
+        if problem.isGoalState(cur_state):
+            return path
+
+        valid_neighbor = filter(lambda x: x[0] not in visited, problem.getSuccessors(cur_state))
+        for nxt in valid_neighbor:
+            npath = path + [nxt[1]]
+            np = problem.getCostOfActions(npath) + heuristic(nxt[0], problem)
+            # Update both path and priority queue
+            if nxt[0] not in res or (nxt[0] in res and problem.getCostOfActions(res[nxt[0]]) > np):
+                res[nxt[0]] = npath
+            pq.update(nxt[0], np)
+    # If no valid path can be found, return empty path
+    return []
+
+
+
+def biDirectionalSearchMM0(problem):
+    """
+    Bi directional search - MM0.
+    Two simple BFS in both direction.
+    """
+    def __reversedPath(p):
+        """
+        Given a action list, return the reversed version of it.
+        """
+        return [Directions.REVERSE[x] for x in p][::-1]
+
+    from util import Queue
+    # Init two ques and visited sets.
+    que1, que2 = Queue(), Queue()
+    visited1, visited2 = dict(), dict()
+
+    que1.push((problem.getStartState(), [], 0))
+    que2.push((problem.goal, [], 0))
+    visited1[problem.getStartState()] = ''
+    visited2[problem.goal] = ''
+    expanding_level1, expanding_level2 = 0, 0
+
+    # Two simple BFS in each while round
+    while True:
+        # First BFS, from start to goal
+        # Will expand one level of nodes
+        if que1.isEmpty():
+            return []
+        while (not que1.isEmpty()) and que1.list[0][2] == expanding_level1:
+            # Get current state
+            cur_state, path, level = que1.pop()
+
+            # Check result
+            if problem.isGoalStateBi(cur_state, visited2):
+                return path + __reversedPath(visited2[cur_state])
+
+            # Expand valid neighbors
+            valid_neighbor = filter(lambda x: x[0] not in visited1, problem.getSuccessors(cur_state))
+            for nxt in valid_neighbor:
+                que1.push((nxt[0], path+[nxt[1]], level+1))
+                visited1[nxt[0]] = path+[nxt[1]]
+        expanding_level1 += 1
+
+        # Second BFS, from foal to start
+        # Will expand one level of nodes
+        if que2.isEmpty():
+            return []
+        while (not que2.isEmpty()) and que2.list[0][2] == expanding_level2:
+            # Get current state
+            cur_state, path, level = que2.pop()
+
+            # Check result
+            if problem.isGoalStateBi(cur_state, visited1):
+                return __reversedPath(visited1[cur_state]) + path
+
+            # Expand valid neighbors
+            valid_neighbor = filter(lambda x: x[0] not in visited2, problem.getSuccessors(cur_state))
+            for nxt in valid_neighbor:
+                que2.push((nxt[0], path+[nxt[1]], level+1))
+                visited2[nxt[0]] = path+[nxt[1]]
+        expanding_level2 += 1
+    return []
+
+
+def biDirectionalSearchMM(problem, heuristic):
+    """
+    Bi directional search - MM.
+    Two Astar search in two directions.
+    """
+    def __reversedPath(p):
+        """
+        Given a action list, return the reversed version of it.
+        """
+        return [Directions.REVERSE[x] for x in p][::-1]
+
+    from util import PriorityQueue
+    # Init two priority queues and visited dicts
+    pq1, pq2 = PriorityQueue(), PriorityQueue()
+    visited1, visited2 = dict(), dict()
+
+    pq1.push((problem.getStartState(), [], 0), heuristic(problem.getStartState(), problem, 'goal'))
+    pq2.push((problem.goal, [], 0), heuristic(problem.goal, problem, 'start'))
+    visited1[problem.getStartState()] = []
+    visited2[problem.goal] = []
+
+    # Run two Astar search in each while round
+    while True:
+        # First Astar search, from start to goal
+        # Only expand one node
+        if pq1.isEmpty():
+            return []
+        cur_state, path, level = pq1.pop()
+
+        if problem.isGoalStateBi(cur_state, visited2):
+            return path + __reversedPath(visited2[cur_state])
+
+        valid_neighbor = filter(lambda x: x[0] not in visited1, problem.getSuccessors(cur_state))
+        for nxt in valid_neighbor:
+            np = heuristic(nxt[0], problem, 'goal') + problem.getCostOfActions(path+[nxt[1]])
+            pq1.push((nxt[0], path+[nxt[1]], level+1), np)
+            visited1[nxt[0]] = path+[nxt[1]]
+
+        # Second Astar search, from goal to start
+        # Only expand one node
+        if pq2.isEmpty():
+            return []
+
+        cur_state, path, level = pq2.pop()
+
+        if problem.isGoalStateBi(cur_state, visited1):
+            return __reversedPath(visited1[cur_state]) + path
+
+        valid_neighbor = filter(lambda x: x[0] not in visited2, problem.getSuccessors(cur_state))
+        for nxt in valid_neighbor:
+            np = heuristic(nxt[0], problem, 'start') + problem.getCostOfActions(path+[nxt[1]])
+            pq2.push((nxt[0], path+[nxt[1]], level+1), np)
+            visited2[nxt[0]] = path+[nxt[1]]
+
+    return []
+
+# Abbreviations
+bfs = breadthFirstSearch
+dfs = depthFirstSearch
+astar = aStarSearch
+bd0 = biDirectionalSearchMM0
+bd = biDirectionalSearchMM
+
+
---
+++ searchAgents.py
@@ -0,0 +1,274 @@
+# searchAgents.py
+# ---------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+#
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+This file contains all of the agents that can be selected to control Pacman.  To
+select an agent, use the '-p' option when running pacman.py.  Arguments can be
+passed to your agent using '-a'.  For example, to load a SearchAgent that uses
+depth first search (dfs), run the following command:
+
+> python pacman.py -p SearchAgent -a fn=depthFirstSearch
+
+Commands to invoke other search strategies can be found in the project
+description.
+
+--------------------------------------------------------------------------------
+Add by CSE571 Team project team:
+    We deleted or modified some agent classes and problem classes.
+    Make sure you only use agent: SearchAgent and problem: PositionSearchProblem.
+    We also deleted some layout files.
+    Make sure you only use layout: tinyMaze, smallMaze, mediumMaze and bigMaze.
+--------------------------------------------------------------------------------
+"""
+
+from game import Directions
+from game import Agent
+from game import Actions
+import util
+import time
+import search
+
+class SearchAgent(Agent):
+    """
+    This very general search agent finds a path using a supplied search
+    algorithm for a supplied search problem, then returns actions to follow that
+    path.
+
+    As a default, this agent runs DFS on a PositionSearchProblem to find
+    location (1,1)
+
+    Options for fn include:
+      depthFirstSearch or dfs
+      breadthFirstSearch or bfs
+
+    """
+
+    def __init__(self, fn='depthFirstSearch', prob='PositionSearchProblem', heuristic='nullHeuristic'):
+        # Warning: some advanced Python magic is employed below to find the right functions and problems
+
+        # Get the search function from the name and heuristic
+        if fn not in dir(search):
+            raise AttributeError, fn + ' is not a search function in search.py.'
+        func = getattr(search, fn)
+        if 'heuristic' not in func.func_code.co_varnames:
+            print('[SearchAgent] using function ' + fn)
+            self.searchFunction = func
+        else:
+            if heuristic in globals().keys():
+                heur = globals()[heuristic]
+            elif heuristic in dir(search):
+                heur = getattr(search, heuristic)
+            else:
+                raise AttributeError, heuristic + ' is not a function in searchAgents.py or search.py.'
+            print('[SearchAgent] using function %s and heuristic %s' % (fn, heuristic))
+            # Note: this bit of Python trickery combines the search algorithm and the heuristic
+            self.searchFunction = lambda x: func(x, heuristic=heur)
+
+        # Get the search problem type from the name
+        if prob not in globals().keys() or not prob.endswith('Problem'):
+            raise AttributeError, prob + ' is not a search problem type in SearchAgents.py.'
+        self.searchType = globals()[prob]
+        print('[SearchAgent] using problem type ' + prob)
+
+    def registerInitialState(self, state):
+        """
+        This is the first time that the agent sees the layout of the game
+        board. Here, we choose a path to the goal. In this phase, the agent
+        should compute the path to the goal and store it in a local variable.
+        All of the work is done in this method!
+
+        state: a GameState object (pacman.py)
+        """
+        if self.searchFunction == None: raise Exception, "No search function provided for SearchAgent"
+        starttime = time.time()
+        problem = self.searchType(state) # Makes a new search problem
+        self.actions  = self.searchFunction(problem) # Find a path
+        totalCost = problem.getCostOfActions(self.actions)
+        print('Path found with total cost of %d in %.1f ms' % (totalCost, (time.time() - starttime)*1000))
+        if '_expanded' in dir(problem): print('Search nodes expanded: %d' % problem._expanded)
+
+    def getAction(self, state):
+        """
+        Returns the next action in the path chosen earlier (in
+        registerInitialState).  Return Directions.STOP if there is no further
+        action to take.
+
+        state: a GameState object (pacman.py)
+        """
+        if 'actionIndex' not in dir(self): self.actionIndex = 0
+        i = self.actionIndex
+        self.actionIndex += 1
+        if i < len(self.actions):
+            return self.actions[i]
+        else:
+            return Directions.STOP
+
+class PositionSearchProblem(search.SearchProblem):
+    """
+    A search problem defines the state space, start state, goal test, successor
+    function and cost function.  This search problem can be used to find paths
+    to a particular point on the pacman board.
+
+    The state space consists of (x,y) positions in a pacman game.
+    """
+
+    def __init__(self, gameState, costFn = lambda x: 1, goal=(1,1), start=None, warn=True, visualize=True):
+        """
+        Stores the start and goal.
+
+        gameState: A GameState object (pacman.py)
+        costFn: A function from a search state (tuple) to a non-negative number
+        goal: A position in the gameState
+        """
+        self.walls = gameState.getWalls()
+        self.startState = gameState.getPacmanPosition()
+        self.startingGameState = gameState
+        if start != None: self.startState = start
+        self.goal = goal
+        self.costFn = costFn
+        self.visualize = visualize
+        if warn and (gameState.getNumFood() != 1 or not gameState.hasFood(*goal)):
+            print 'Warning: this does not look like a regular search maze'
+
+        # For display purposes
+        self._visited, self._visitedlist, self._expanded = {}, [], 0 # DO NOT CHANGE
+
+    def getStartState(self):
+        return self.startState
+
+    def isGoalState(self, state):
+        isGoal = state == self.goal
+
+        # For display purposes only
+        if isGoal and self.visualize:
+            self._visitedlist.append(state)
+            import __main__
+            if '_display' in dir(__main__):
+                if 'drawExpandedCells' in dir(__main__._display): #@UndefinedVariable
+                    __main__._display.drawExpandedCells(self._visitedlist) #@UndefinedVariable
+
+        return isGoal
+
+    def isGoalStateBi(self, state, visited):
+        """
+        A new isGoal() method used for bi-directonal search algorithm.
+        """
+        isGoal = state in visited
+
+        # For display purposes only
+        if isGoal and self.visualize:
+            self._visitedlist.append(state)
+            import __main__
+            if '_display' in dir(__main__):
+                if 'drawExpandedCells' in dir(__main__._display): #@UndefinedVariable
+                    __main__._display.drawExpandedCells(self._visitedlist) #@UndefinedVariable
+
+        return isGoal
+
+    def getSuccessors(self, state):
+        """
+        Returns successor states, the actions they require, and a cost of 1.
+
+         As noted in search.py:
+             For a given state, this should return a list of triples,
+         (successor, action, stepCost), where 'successor' is a
+         successor to the current state, 'action' is the action
+         required to get there, and 'stepCost' is the incremental
+         cost of expanding to that successor
+        """
+
+        successors = []
+        for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
+            x,y = state
+            dx, dy = Actions.directionToVector(action)
+            nextx, nexty = int(x + dx), int(y + dy)
+            if not self.walls[nextx][nexty]:
+                nextState = (nextx, nexty)
+                cost = self.costFn(nextState)
+                successors.append( ( nextState, action, cost) )
+
+        # Bookkeeping for display purposes
+        self._expanded += 1 # DO NOT CHANGE
+        if state not in self._visited:
+            self._visited[state] = True
+            self._visitedlist.append(state)
+
+        return successors
+
+    def getCostOfActions(self, actions):
+        """
+        Returns the cost of a particular sequence of actions. If those actions
+        include an illegal move, return 999999.
+        """
+        if actions == None: return 999999
+        x,y= self.getStartState()
+        cost = 0
+        for action in actions:
+            # Check figure out the next state and see whether its' legal
+            dx, dy = Actions.directionToVector(action)
+            x, y = int(x + dx), int(y + dy)
+            if self.walls[x][y]: return 999999
+            cost += self.costFn((x,y))
+        return cost
+
+def foodHeuristic(state, problem, type='goal'):
+    """
+    Your heuristic for the FoodSearchProblem goes here.
+
+    This heuristic must be consistent to ensure correctness.  First, try to come
+    up with an admissible heuristic; almost all admissible heuristics will be
+    consistent as well.
+
+    If using A* ever finds a solution that is worse uniform cost search finds,
+    your heuristic is *not* consistent, and probably not admissible!  On the
+    other hand, inadmissible or inconsistent heuristics may find optimal
+    solutions, so be careful.
+
+    The state is a tuple ( pacmanPosition, foodGrid ) where foodGrid is a Grid
+    (see game.py) of either True or False. You can call foodGrid.asList() to get
+    a list of food coordinates instead.
+
+    If you want access to info like walls, capsules, etc., you can query the
+    problem.  For example, problem.walls gives you a Grid of where the walls
+    are.
+
+    If you want to *store* information to be reused in other calls to the
+    heuristic, there is a dictionary called problem.heuristicInfo that you can
+    use. For example, if you only want to count the walls once and store that
+    value, try: problem.heuristicInfo['wallCount'] = problem.walls.count()
+    Subsequent calls to this heuristic can access
+    problem.heuristicInfo['wallCount']
+    """
+    if type == 'goal':
+        return mazeDistance(state, problem.goal, problem.startingGameState)
+    else:
+        return mazeDistance(state, problem.startState, problem.startingGameState)
+
+def mazeDistance(point1, point2, gameState):
+    """
+    Returns the maze distance between any two points, using the search functions
+    you have already built. The gameState can be any game state -- Pacman's
+    position in that state is ignored.
+
+    Example usage: mazeDistance( (2,4), (5,6), gameState)
+
+    This might be a useful helper function for your ApproximateSearchAgent.
+    """
+    x1, y1 = point1
+    x2, y2 = point2
+    walls = gameState.getWalls()
+    assert not walls[x1][y1], 'point1 is a wall: ' + str(point1)
+    assert not walls[x2][y2], 'point2 is a wall: ' + str(point2)
+    prob = PositionSearchProblem(gameState, start=point1, goal=point2, warn=False, visualize=False)
+    return len(search.bfs(prob))
